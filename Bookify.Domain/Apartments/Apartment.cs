2d61f9654b4d267fbe35690ad6b64c64f89c3bf7
fealves <franciscomcalves@gmail.com>
2024-08-23 17:55:46 -0300

    Clean Architecture initial commit - Bookify with Apartments rich model

---
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -0,0 +1,39 @@
+﻿using Bookify.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Apartments
+{
+    internal sealed class Apartment : Entity
+    {
+        public Apartment(Guid id,
+            Name name,
+            Description description,
+            Address address,
+            Money price,
+            Money cleaningFee,
+            List<Amenities> amenities            
+            ) : base(id)
+        {
+            Name = name;
+            Description = description;
+            Address = address;
+            Price = price;
+            CleaningFee = cleaningFee;
+            Amenities = amenities;
+        }
+
+        public Name Name { get; private set; }
+        public Description Description { get; private set; }
+        public Address Address { get; private set; }
+        public Money Price { get; private set; }
+        public Money CleaningFee { get; private set; }
+        public DateTime? LastBookedOnUtc { get; private set; }
+
+        public List<Amenities> Amenities { get; private set; } = new();
+
+    }
+}
---
+++ Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Apartments
+{
+    public record Currency
+    {
+        public static readonly Currency None = new Currency("", "");
+        public static readonly Currency Usd = new Currency("USD", "$");
+        public static readonly Currency Eur = new Currency("EUR", "€");
+        public static readonly Currency Real = new Currency("BRL", "R$");
+        private Currency(string code, string symbol)
+        {
+            Code = code;
+            Symbol = symbol;
+        }
+        public string Code { get; init; }
+        public string Symbol { get; init; }
+
+        public static IReadOnlyCollection<Currency> All = new[] { Usd, Eur, Real };
+        public static Currency FromCode(string code)
+        {
+            return All.FirstOrDefault(c => c.Code == code) ?? throw new ApplicationException("The currency code is invalid!");
+        }
+
+    }
+
+}
